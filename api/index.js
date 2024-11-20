ffe68a99fffc2521b088b0385b9e2821b930fad9
David Vidal <dvidalv@gmail.com>
2024-11-19 20:17:06 -0600

    cors and more

--- api/index.js
+++ api/index.js
@@ -1,12 +1,34 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+
+const app = express();
+app.use(express.json());
+
+const corsOptions = {
+	allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+	origin: [
+		'http://localhost:3000',
+		'http://localhost:3001',
+		'http://localhost:5173',
+		'http://localhost:5174',
+	],
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	preflightContinue: false,
+	optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+	res.send('Express on Vercel');
 });
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
