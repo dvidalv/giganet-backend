5dc2990821e4c6a0103ff0cc58ec6980c5b310c3
David Vidal <dvidalv@gmail.com>
2024-11-19 20:05:23 -0600

    first commit

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
+
+module.exports = app;
