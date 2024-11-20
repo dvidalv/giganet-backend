a1b78e47cf2a3bcad8ac892af888e239dcd45aca
David Vidal <dvidalv@gmail.com>
2024-11-19 20:25:22 -0600

    adding vercel dominio to cors

--- api/index.js
+++ api/index.js
@@ -13,6 +13,7 @@ const corsOptions = {
 		'http://localhost:3001',
 		'http://localhost:5173',
 		'http://localhost:5174',
+		'https://giganet-backend.vercel.app',
 	],
 	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 	preflightContinue: false,
@@ -23,6 +24,10 @@ app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
 	res.send('Express on Vercel');
+});
+
+app.get('/api/v1/comprobantes', (req, res) => {
+	res.json({ code: 200, message: 'Comprobante', data: {} });
 });
 
 const PORT = process.env.PORT || 3001;
