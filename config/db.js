0e9174de3dc47fa864ff3d45252adcb1e715d331
David Vidal <dvidalv@gmail.com>
2025-01-12 10:56:08 -0400

    mod mongodb conexion

--- config/db.js
+++ config/db.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGODB_URI);
+const uri = process.env.MONGODB_URI;
 
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
-	} catch (error) {
-		console.error(`Error: ${error.message}`);
-		process.exit(1);
-	}
+const connectDB = async () => {
+	// console.log(uri);
+
+	try {
+    await mongoose.connect(uri);
+    console.log('Conexión exitosa a MongoDB Atlas');
+    console.log(mongoose.connection.name);
+  } catch (e) {
+    console.error('Error al conectar a MongoDB Atlas', e);
+  }
+
+	// try {
+	// 	const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+	// 	console.log(`MongoDB Connected: ${conn.connection.host}`);
+	// } catch (error) {
+	// 	console.error(`Error: ${error.message}`);
+	// 	process.exit(1);
+	// }
 };
 
 module.exports = connectDB;
