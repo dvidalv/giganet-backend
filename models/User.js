597c13832fba47f2d27d76e67280ced8061ce7e0
David Vidal <dvidalv@gmail.com>
2025-01-12 15:08:42 -0400

    change to diferente couster

--- models/User.js
+++ models/User.js
@@ -45,4 +45,4 @@ userSchema.pre('save', async function (next) {
 	next();
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema, 'usuarios');
